ef14ebccc26682e565bf413ca8c8af84d78cfe8e
eedu7 <mueedahmad067@gmail.com>
2024-09-12 15:07:00 +0500

    Initial Project Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./sections/Navbar.jsx";
-
+import Hero from "./sections/Hero.jsx";
+/*
+* https://youtu.be/kt0FrkQgw8w?t=7879
+* */
 const App = () => {
     return (
         <main className="max-w-7xl mx-auto">
             <Navbar/>
+            <Hero />
         </main>
     )
 }

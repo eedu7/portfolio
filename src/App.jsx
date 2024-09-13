485d3086f000ec130eb7105c3d1c93e32d29fe7d
eedu7 <mueedahmad067@gmail.com>
2024-09-13 20:28:17 +0500

    MINOR: added the rings, cube, react logo and target model

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
-/*
-* https://youtu.be/kt0FrkQgw8w?t=7879
-* */
+
 const App = () => {
     return (
         <main className="max-w-7xl mx-auto">
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,28 +2,21 @@ import {Canvas} from "@react-three/fiber";
 import {PerspectiveCamera} from "@react-three/drei";
 import {Suspense} from "react";
 import CanvasLoader from "../components/Loading.jsx";
-import {Leva, useControls} from "leva";
 import {HackerRoom} from "../components/HackerRoom.jsx";
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../../constants/index.js";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Rings.jsx";
 
 const Hero = () => {
 
-    const controls = useControls("HackerRoom", {
-        positionX: {
-            value: 2.5, min: -10, max: 10
-        }, positionY: {
-            value: 2.5, min: -10, max: 10
-        }, positionZ: {
-            value: 2.5, min: -10, max: 10
-        }, rotationX: {
-            value: 0, min: -10, max: 10
-        }, rotationY: {
-            value: 0, min: -10, max: 10
-        }, rotationZ: {
-            value: 0, min: -10, max: 10
-        }, scale: {
-            value: 0.07, min:0, max:1
-        }
-    })
+    const isSmall = useMediaQuery({maxWidth: 440});
+    const isMobile = useMediaQuery({maxWidth: 768});
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
     return (<section className="min-h-screen w-full flex flex-col relative">
         <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -32,19 +25,22 @@ const Hero = () => {
             <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
         </div>
         <div className="w-full h-full absolute inset-0">
-            <Leva/>
+            {/*<Leva/>*/}
             <Canvas className="w-full h-full">
                 <Suspense fallback={<CanvasLoader/>}>
-                    <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+                    <PerspectiveCamera makeDefault position={[0, 0, 20]}/>
 
                     <HackerRoom
-                        // scale={0.07}
-                        // position={[0, 0, 0]}
-                        // rotation={[0.1, -Math.PI / 2, 0]}
-                        position={[controls.positionX, controls.positionY, controls.positionZ]}
-                        rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
-                        scale={[controls.scale, controls.scale, controls.scale]}
+                        scale={sizes.deskScale}
+                        position={sizes.deskPosition}
+                        rotation={[0, -Math.PI, 0]}
                     />
+                    <group>
+                        <Target position={sizes.targetPosition} rotation={[0, Math.PI / 5, 0]}/>
+                        <ReactLogo position={sizes.reactLogoPosition} />
+                        <Cube position={sizes.cubePosition} />
+                        <Rings position={sizes.ringPosition} />
+                    </group>
 
                     <ambientLight intensity={1}/>
                     <directionalLight position={[10, 10, 10]} intensity={0.5}/>
